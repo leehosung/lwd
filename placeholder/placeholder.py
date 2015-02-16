858b0dd55707cd20dee0fa91c0a4286cf3a068a8
Lee Ho Sung <lee.ho.sung@gmail.com>
2015-02-16 14:06:53 +0900

    finish chapter2

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -8,6 +8,8 @@ DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
 
+BASE_DIR = os.path.dirname(__file__)
+
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
 settings.configure(
@@ -20,13 +22,26 @@ settings.configure(
         'django.middleware.csrf.CsrfViewMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
+    INSTALLED_APPS=(
+        'django.contrib.staticfiles',
+    ),
+    TEMPLATE_DIRS=(
+        os.path.join(BASE_DIR, 'templates'),
+    ),
+    STATICFILES_DIRS=(
+        os.path.join(BASE_DIR, 'static'),
+    ),
+    STATIC_URL='/static/',
 )
+
 
 from django import forms
 from django.conf.urls import url
 from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
+from django.shortcuts import render
 from django.views.decorators.http import etag
 from io import BytesIO
 from PIL import Image, ImageDraw
@@ -75,7 +90,9 @@ def placeholder(request, width, height):
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    example = reverse('placeholder', kwargs={'width': 50, 'height': 50})
+    context = {'example': request.build_absolute_uri(example)}
+    return render(request, 'home.html', context)
 
 
 urlpatterns = (
