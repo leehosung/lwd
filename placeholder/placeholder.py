5c4ae66a4948a42287a03ebe4896e5d31cce1d76
Lee Ho Sung <lee.ho.sung@gmail.com>
2015-02-16 13:03:37 +0900

    can view placeholder image

--- placeholder/placeholder.py
+++ placeholder/placeholder.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 DEBUG = os.environ.get('DEBUG', 'on') == 'on'
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'jg=xo4)c%paq4hgc#4=$!f85pduf_d$d+qi==w_-vc!r&1de-%')
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
@@ -21,16 +21,54 @@ settings.configure(
     ),
 )
 
-from django.core.wsgi import get_wsgi_application
+from django import forms
 from django.conf.urls import url
-from django.http import HttpResponse
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse, HttpResponseBadRequest
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image."""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate(self, image_format='PNG'):
+        """Generate an image of the fiven type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        draw = ImageDraw.Draw(image)
+        text = '{} X {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if textwidth < width and textheight < height:
+            texttop = (height - textheight) // 2
+            textleft = (width - textwidth) // 2
+            draw.text((textleft, texttop), text, fill=(255, 255, 255))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
+
+def placeholder(request, width, height):
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        image = form.generate()
+        return HttpResponse(image, content_type='image/png')
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 
 def index(request):
     return HttpResponse('Hello World')
 
+
 urlpatterns = (
-    url(r'^$', index),
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder, name='placeholder'),
+    url(r'^$', index, name='homepage'),
 )
 
 application = get_wsgi_application()
